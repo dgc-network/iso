cc89f9f0f86060d385c85031e7918f93b6981e24
Rover Chen <rover.k.chen@gmail.com>
2024-01-06 14:02:56 +0800

    function register_session() {     if ( ! session_id() ) {         session_start();     } } add_action( 'init', 'register_session' );

--- iso.php
+++ iso.php
@@ -17,16 +17,24 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
+if (!current_user_can('administrator')) {
+    add_filter('show_admin_bar', '__return_false');
+}
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Function to enqueue scripts for the custom chat_gpt admin
+function register_session() {
+    if ( ! session_id() ) {
+        session_start();
+    }
+}
+add_action( 'init', 'register_session' );
+
 function admin_enqueue() {
     wp_enqueue_style( 'jquery-ui-style', 'https://code.jquery.com/ui/1.13.2/themes/smoothness/jquery-ui.css' );
     wp_enqueue_script( 'jquery-ui', 'https://code.jquery.com/ui/1.13.2/jquery-ui.js', array('jquery'), null, true);
-    //wp_enqueue_script( 'jquery-ui-datepicker' );
-    //wp_enqueue_script( 'jquery-ui-dialog' );
     wp_enqueue_style( 'admin-enqueue-css', plugins_url( 'assets/css/admin-enqueue.css' , __FILE__ ), '', time() );
     wp_enqueue_script( 'admin-enqueue-js', plugins_url( 'assets/js/admin-enqueue.js' , __FILE__ ), array( 'jquery' ), time() );
     wp_localize_script( 'admin-enqueue-js', 'ajax_object', array( 
@@ -39,11 +47,6 @@ add_action('admin_enqueue_scripts', 'admin_enqueue');
 function wp_enqueue() {
     wp_enqueue_style( 'jquery-ui-css', 'https://code.jquery.com/ui/1.13.2/themes/smoothness/jquery-ui.css' );
     wp_enqueue_script( 'jquery-ui', 'https://code.jquery.com/ui/1.13.2/jquery-ui.js', array('jquery'), null, true);
-
-    //wp_enqueue_script( 'jquery-ui-sortable' );
-    //wp_enqueue_script( 'jquery-ui-datepicker' );
-    //wp_enqueue_script( 'jquery-ui-dialog' );
-    
     wp_enqueue_style( 'wp-enqueue-css', plugins_url( 'assets/css/wp-enqueue.css' , __FILE__ ), '', time() );
     wp_enqueue_script( 'wp-enqueue-js', plugins_url( 'assets/js/wp-enqueue.js' , __FILE__ ), array( 'jquery' ), time() );
     wp_localize_script( 'wp-enqueue-js', 'ajax_object', array( 
@@ -64,18 +67,10 @@ require_once plugin_dir_path( __FILE__ ) . 'includes/admin-edit-site.php';
 require_once plugin_dir_path( __FILE__ ) . 'includes/admin-edit-document.php';
 require_once plugin_dir_path( __FILE__ ) . 'includes/display-documents.php';
 require_once plugin_dir_path( __FILE__ ) . 'includes/to-do-list.php';
-//require_once plugin_dir_path( __FILE__ ) . 'redirect-line-login.php';
 
 add_option('_line_account', 'https://line.me/ti/p/@804poufw');
 add_option('_operation_fee_rate', 0.005);
 add_option('_operation_wallet_address', 'DKVr5kVFcDDREPeLSDvUcNbXAffdYuPQCd');
-
-function register_session() {
-    if ( ! session_id() ) {
-        session_start();
-    }
-}
-add_action( 'init', 'register_session' );
 
 // Function to check if the string is a valid URL
 function isURL($str) {
