034f8f38b59e1275a70d1185b88106aef710e5be
Rover Chen <rover.k.chen@gmail.com>
2024-01-08 17:17:00 +0800

    <?php the_ID();?>

--- includes/display-documents.php
+++ includes/display-documents.php
@@ -77,7 +77,7 @@ function display_documents_shortcode() {
                         <td style="text-align:center;"><span id="btn-edit-document-<?php echo $post_id;?>" class="dashicons dashicons-edit"></span></td>
                         <td><a href="<?php echo $doc_url;?>"><?php the_title();?></a></td>
                         <td style="text-align:center;"><?php echo esc_html(get_post_meta($post_id, 'doc_number', true)).$post_id;?></td>
-                        <td style="text-align:center;"><?php echo esc_html(get_post_meta($post_id, 'doc_revision', true));?></td>
+                        <td style="text-align:center;"><?php echo esc_html(get_post_meta($post_id, 'doc_revision', true));?><?php the_ID();?></td>
                         <td style="text-align:center;"><?php echo esc_html(get_post_meta($post_id, 'doc_date', true));?></td>
                         <td style="text-align:center;"><span id="btn-del-document-<?php the_ID();?>" class="dashicons dashicons-trash"></span></td>
                     </tr>
