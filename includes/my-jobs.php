3ebeb78620b5d4376a970c92ae610cdda8c98ad2
Rover Chen <rover.k.chen@gmail.com>
2024-01-15 10:18:33 +0800

    $options .= '<option value="0">'.__( '發行', 'your-text-domain' ).'</option>';

--- includes/my-jobs.php
+++ includes/my-jobs.php
@@ -298,7 +298,7 @@ function select_site_job_option_data($selected_job='', $site_id=0) {
         $options .= '<option value="' . esc_attr(get_the_ID()) . '" '.$selected.' />' . esc_html(get_the_title()) . '</option>';
     endwhile;
     wp_reset_postdata(); // Reset post data to the main loop
-    if ($selected_job=0){
+    if ($selected_job==0){
         $options .= '<option value="0" selected>'.__( '發行', 'your-text-domain' ).'</option>';
     } else {
         $options .= '<option value="0">'.__( '發行', 'your-text-domain' ).'</option>';
