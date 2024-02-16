3b07c4b13fe3b4d879117d7be3c698ae2ba51fce
Rover Chen <rover.k.chen@gmail.com>
2024-02-16 14:36:13 +0800

    html

--- assets/js/display-documents.js
+++ assets/js/display-documents.js
@@ -430,7 +430,7 @@ jQuery(document).ready(function($) {
                     //$("#listing-style").empty().append(response.listing_style);
                     //$("#editing-type").empty().append(response.editing_type);
                     $("#listing-style").html(response.listing_style);
-                    $("#editing-type").html(response.editing_type);
+                    //$("#editing-type").html(response.editing_type);
                     $("#default-value").val(response.default_value);
                     //$('#is-listing').prop('checked', response.is_listing == 1);
                     //$('#is-editing').prop('checked', response.is_editing == 1);
