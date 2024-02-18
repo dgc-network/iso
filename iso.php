ee77b7d0ec64e0147659ff107e18935e4155ced2
Rover Chen <rover.k.chen@gmail.com>
2024-02-18 22:15:30 +0800

    exit;

--- iso.php
+++ iso.php
@@ -350,17 +350,19 @@ function init_webhook_events() {
 
     $line_bot_api = new line_bot_api();
     $open_ai_api = new open_ai_api();
-/*
+
     if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
         http_response_code(405);
         error_log('Method not allowed');
+        exit;
     }
-*/
+
     $entityBody = file_get_contents('php://input');
 
     if ($entityBody === false || strlen($entityBody) === 0) {
         http_response_code(400);
         error_log('Missing request body');
+        exit;
     }
 
     $data = json_decode($entityBody, true);
@@ -373,10 +375,10 @@ function init_webhook_events() {
     }
     
     //$data = listen_webhook();
-    //foreach ((array)$data['events'] as $event) {
+    foreach ((array)$data['events'] as $event) {
 
-    $events = $line_bot_api->parseEvents();
-    foreach ((array)$events as $event) {
+    //$events = $line_bot_api->parseEvents();
+    //foreach ((array)$events as $event) {
 
         // Start the User Login/Registration process if got the one time password
         if (esc_attr((int)$event['message']['text'])==esc_attr((int)get_option('_one_time_password'))) {
