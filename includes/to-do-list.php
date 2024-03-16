031fa869ec9bb5588b51d9b7793aaedbf10b34f4
Rover Chen <rover.k.chen@gmail.com>
2024-03-16 10:45:14 +0800

    echo display_todo_dialog(sanitize_text_field($_GET['_id']));

--- includes/to-do-list.php
+++ includes/to-do-list.php
@@ -59,8 +59,10 @@ add_action('init', 'register_action_post_type');
 function to_do_list_shortcode() {
     // Check if the user is logged in
     if (is_user_logged_in()) {
+        if (isset($_GET['_id'])) echo display_todo_dialog(sanitize_text_field($_GET['_id']));
+
         if (isset($_GET['_id'])) {
-            display_todo_dialog(sanitize_text_field($_GET['_id']));
+            echo display_todo_dialog(sanitize_text_field($_GET['_id']));
         } else {
             if ($_GET['_select_todo']=='1') display_signature_record();
             if ($_GET['_select_todo']!='1') display_to_do_list();    
