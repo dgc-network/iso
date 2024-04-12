cc97002df4e58a0a5a66c205695a08046ff700b9
Rover Chen <rover.k.chen@gmail.com>
2024-04-13 05:38:23 +0800

    $text_message,

--- iso.php
+++ iso.php
@@ -244,7 +244,13 @@ function init_webhook_events() {
 
                                     $line_bot_api->replyMessage([
                                         'replyToken' => $event['replyToken'],
-                                        'messages' => [$flexMessage],
+                                        //'messages' => [$flexMessage],
+                                        'messages' => [
+                                            [
+                                                'type' => 'text',
+                                                'text' => $text_message,
+                                            ]                                                                    
+                                        ]
                                     ]);
                                 }
                             }
