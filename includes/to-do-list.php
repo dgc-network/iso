2d311482f3de0590514cf882d8dc8c19186e36ac
Rover Chen <rover.k.chen@gmail.com>
2024-04-13 17:17:25 +0800

    array(             'key'     => 'start_job',             'compare' => 'EXISTS', // Check if the meta key exists         ),

--- includes/to-do-list.php
+++ includes/to-do-list.php
@@ -209,19 +209,13 @@ function retrieve_todo_list_data(){
                 ),
                 array(
                     'key'     => 'start_job',
-                    'compare' => 'EXISTS', // Check if the meta key exists
-                ),
-                array(
-                    'key'     => 'start_job',
                     'value'   => $user_job_ids, // User's job IDs
                     'compare' => 'IN',
                 ),
-/*                
                 array(
                     'key'     => 'todo_status',
                     'compare' => 'NOT EXISTS',
                 ),
-*/                
             ),
         );
 
