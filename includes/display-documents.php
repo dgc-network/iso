244e805c13a36f178ec1633203f73414028a6d68
Rover Chen <rover.k.chen@gmail.com>
2024-05-16 16:42:25 +0800

    <?php echo $job_title;?>

--- includes/display-documents.php
+++ includes/display-documents.php
@@ -459,9 +459,7 @@ if (!class_exists('display_documents')) {
             <div class="entry-content wp-block-post-content has-global-padding is-layout-constrained wp-block-post-content-is-layout-constrained">
                 <div class="mermaid">
                 sequenceDiagram
-                    participant <?php echo $job_title;?> 
-                    participant Bob
-                    Alice->>John: Hello John, how are you?
+                    <?php echo $job_title;?>->><?php echo $job_number;?>: <?php echo $doc_revision;?> 
                     loop Healthcheck
                         John->>John: Fight against hypochondria
                     end
