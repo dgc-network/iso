055616dd2625ece5fb95807c94182de7aa1616d3
Rover Chen <rover.k.chen@gmail.com>
2024-05-16 16:13:27 +0800

    <div class="entry-content wp-block-post-content has-global-padding is-layout-constrained wp-block-post-content-is-layout-constrained">

--- includes/display-documents.php
+++ includes/display-documents.php
@@ -13,7 +13,7 @@ if (!class_exists('display_documents')) {
             add_action( 'init', array( $this, 'register_doc_report_post_type' ) );
             add_action( 'init', array( $this, 'register_doc_field_post_type' ) );
             add_action( 'init', array( $this, 'register_doc_category_post_type' ) );
-            add_action('wp_footer', array( $this, 'add_mermaid_js'));
+            //add_action('wp_footer', array( $this, 'add_mermaid_js'));
 
             add_action( 'wp_ajax_get_document_dialog_data', array( $this, 'get_document_dialog_data' ) );
             add_action( 'wp_ajax_nopriv_get_document_dialog_data', array( $this, 'get_document_dialog_data' ) );
