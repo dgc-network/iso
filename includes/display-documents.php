ab811e6da631e28e7daf7478ff574423d61c326d
Rover Chen <rover.k.chen@gmail.com>
2024-05-16 20:16:14 +0800

    $html = ob_get_clean();             return $html;

--- includes/display-documents.php
+++ includes/display-documents.php
@@ -417,6 +417,7 @@ if (!class_exists('display_documents')) {
         }
         
         function display_mermaid_drawing($doc_id=false) {
+            ob_start();
             $profiles_class = new display_profiles();
             $query = $profiles_class->retrieve_doc_action_list_data($doc_id);
             if ($query->have_posts()) :
@@ -435,6 +436,8 @@ if (!class_exists('display_documents')) {
                 endwhile;
                 wp_reset_postdata();
             endif;    
+            $html = ob_get_clean();
+            return $html;
 
         }
         
@@ -511,7 +514,7 @@ if (!class_exists('display_documents')) {
                         $action_content = get_post_field('post_content', get_the_ID());
                         $next_job = get_post_meta(get_the_ID(), 'next_job', true);
                         $next_job_title = get_the_title($next_job);
-                        if ($next_job>0) $this->display_mermaid_drawing($next_job);
+                        if ($next_job>0) echo $this->display_mermaid_drawing($next_job);
                         if ($next_job==-1) $next_job_title = __( '發行', 'your-text-domain' );
                         if ($next_job==-2) $next_job_title = __( '廢止', 'your-text-domain' );
                         $next_leadtime = get_post_meta(get_the_ID(), 'next_leadtime', true);
