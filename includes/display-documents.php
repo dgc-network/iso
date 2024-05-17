aff68ff441fe3254bc9099b4a0f11bb3ad57d6e6
Rover Chen <rover.k.chen@gmail.com>
2024-05-17 19:37:01 +0800

    if ($next_job==-1) {                             $next_job_title = __( '文件發行', 'your-text-domain' );                             if ($is_doc_report) $next_job_title = __( '記錄歸檔', 'your-text-domain' );                         }                         if ($next_job==-2) {                             $next_job_title = __( '文件廢止', 'your-text-domain' );                             if ($is_doc_report) $next_job_title = __( '記錄作廢', 'your-text-domain' );                         }

--- includes/display-documents.php
+++ includes/display-documents.php
@@ -450,9 +450,15 @@ if (!class_exists('display_documents')) {
                         $current_job_title = get_the_title($current_job);
                         $next_job = get_post_meta(get_the_ID(), 'next_job', true);
                         $next_job_title = get_the_title($next_job);
-                        if ($next_job==-1) $next_job_title = __( '發行', 'your-text-domain' );
-                        if ($next_job==-2) $next_job_title = __( '廢止', 'your-text-domain' );
-                        $next_leadtime = get_post_meta(get_the_ID(), 'next_leadtime', true);
+                        $is_doc_report = get_post_meta($next_job, 'is_doc_report', true);
+                        if ($next_job==-1) {
+                            $next_job_title = __( '文件發行', 'your-text-domain' );
+                            if ($is_doc_report) $next_job_title = __( '記錄歸檔', 'your-text-domain' );
+                        }
+                        if ($next_job==-2) {
+                            $next_job_title = __( '文件廢止', 'your-text-domain' );
+                            if ($is_doc_report) $next_job_title = __( '記錄作廢', 'your-text-domain' );
+                        }
                         ?>
                         <?php echo $current_job_title;?>->><?php echo $next_job_title;?>: <?php echo $action_title;?> 
                         <?php
--- includes/display-profiles.php
+++ includes/display-profiles.php
@@ -709,8 +709,14 @@ if (!class_exists('display_profiles')) {
                         $action_content = get_post_field('post_content', get_the_ID());
                         $next_job = get_post_meta(get_the_ID(), 'next_job', true);
                         $next_job_title = get_the_title($next_job);
-                        if ($next_job==-1) $next_job_title = __( '發行', 'your-text-domain' );
-                        if ($next_job==-2) $next_job_title = __( '廢止', 'your-text-domain' );
+                        if ($next_job==-1) {
+                            $next_job_title = __( '文件發行', 'your-text-domain' );
+                            if ($is_doc_report) $next_job_title = __( '記錄歸檔', 'your-text-domain' );
+                        }
+                        if ($next_job==-2) {
+                            $next_job_title = __( '文件廢止', 'your-text-domain' );
+                            if ($is_doc_report) $next_job_title = __( '記錄作廢', 'your-text-domain' );
+                        }
                         $next_leadtime = get_post_meta(get_the_ID(), 'next_leadtime', true);
                         ?>
                         <tr id="edit-doc-action-<?php the_ID();?>">
