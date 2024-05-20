326585cc8c18b0e5367f49c67a9072a73c7d35d1
Rover Chen <rover.k.chen@gmail.com>
2024-05-20 17:22:18 +0800

    retrieve_doc_action_list_data

--- includes/display-profiles.php
+++ includes/display-profiles.php
@@ -736,7 +736,7 @@ if (!class_exists('display_profiles')) {
             <div id="new-doc-action" class="button" style="border:solid; margin:3px; text-align:center; border-radius:5px; font-size:small;">+</div>
             </fieldset>
             </div>
-            <?php //echo $this->display_doc_action_dialog();?>
+            <?php echo $this->display_doc_action_dialog();?>
             <?php
             $html = ob_get_clean();
             return $html;            
