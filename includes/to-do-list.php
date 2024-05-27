5bb2268fb9acfa0955f95b7525e65efc9cee4dc3
Rover Chen <rover.k.chen@gmail.com>
2024-05-27 11:50:15 +0800

    echo '<div class="ui-widget" id="result-container">';                             echo $this->display_todo_dialog($todo_id);                             echo '</div>';

--- includes/to-do-list.php
+++ includes/to-do-list.php
@@ -32,7 +32,10 @@ if (!class_exists('to_do_list')) {
                     if ($submit_time) {
                         echo 'Todo #'.$todo_id.' has been submitted by '.$user->display_name.' on '.wp_date(get_option('date_format'), $submit_time).' '.wp_date(get_option('time_format'), $submit_time);
                     } else {
+                        echo '<div class="ui-widget" id="result-container">';
                         echo $this->display_todo_dialog($todo_id);
+                        echo '</div>';
+/*
                         $doc_id = get_post_meta($todo_id, 'doc_id', true);
                         $report_id = get_post_meta($todo_id, 'report_id', true);
                         if ($report_id) $doc_id = get_post_meta($report_id, 'doc_id', true);
@@ -49,6 +52,7 @@ if (!class_exists('to_do_list')) {
                         } else {
                             echo 'Todo #'.$todo_id.' is not an validaed todo.';
                         }
+*/                        
                     }
                 }
 
