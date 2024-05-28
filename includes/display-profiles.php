20422cefc5f8b9cc3c29e531a24d42d6215ccbf8
Rover Chen <rover.k.chen@gmail.com>
2024-05-28 20:40:01 +0800

    'post_type' => 'PATCH',

--- includes/display-profiles.php
+++ includes/display-profiles.php
@@ -73,9 +73,10 @@ if (!class_exists('display_profiles')) {
                         'company' => 'dg',
                         //'service' => 'Chart_of_Accounts',
                         'service' => 'Customers',
-                        //'post_type' => 'POST', // Change to 'GET' or 'POST' as needed
-                        'post_type' => 'PATCH', // Change to 'GET' or 'POST' as needed
-                        'etag_data' => array( // Include any data you need to send with the POST request
+                        //'post_type' => 'POST',
+                        //'post_type' => 'PATCH',
+                        'post_type' => 'DELETE',
+                        'etag_data' => array( // Include any data you need to send with the GET/PATCH/DELETE request
                             //'Name' => (string) get_post_time('U', true, $site_id),
                             //'No' => (string) time(),
                             //'Name' => 'New customer',
