9b2ed9f2680e212262c0ef58915dfc7f5858a35f
Rover Chen <rover.k.chen@gmail.com>
2024-05-28 16:10:05 +0800

    'post_type' => 'PUT', // Change to 'GET' or 'POST' as needed

--- includes/display-profiles.php
+++ includes/display-profiles.php
@@ -73,12 +73,14 @@ if (!class_exists('display_profiles')) {
                         'company' => 'dg',
                         //'service' => 'Chart_of_Accounts',
                         'service' => 'Customers',
-                        'post_type' => 'POST', // Change to 'GET' or 'POST' as needed
-                        //'post_type' => 'PUT', // Change to 'GET' or 'POST' as needed
+                        //'post_type' => 'POST', // Change to 'GET' or 'POST' as needed
+                        'post_type' => 'PUT', // Change to 'GET' or 'POST' as needed
                         'body_data' => array( // Include any data you need to send with the POST request
                             //'Name' => (string) get_post_time('U', true, $site_id),
-                            'No' => (string) time(),
-                            'Name' => 'New customer',
+                            //'No' => (string) time(),
+                            //'Name' => 'New customer',
+                            'No' => '1716883625',
+                            'Name' => '新客戶',
                             //'Display_Name' => get_the_title($site_id),
                             //'Balance' => 0,
                         ),
