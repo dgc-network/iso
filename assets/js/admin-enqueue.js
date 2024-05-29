83e82b768a5fd8fccabf664de6446c3b8d4f27be
Rover Chen <rover.k.chen@gmail.com>
2024-05-29 21:55:30 +0800

    jQuery(document).ready(function($) {     function fetchMqttMessages() {         $.ajax({             url: mqttClient.ajax_url,             method: 'POST',             data: {                 action: 'get_mqtt_messages',                 nonce: mqttClient.nonce             },             success: function(response) {                 $('#mqtt-messages').html(response);             }         });     }
        // Poll the server every 5 seconds
        setInterval(fetchMqttMessages, 5000);
    
        // Fetch messages immediately on load
        fetchMqttMessages();
    });

--- assets/js/admin-enqueue.js
+++ assets/js/admin-enqueue.js
@@ -134,3 +134,24 @@ jQuery(document).ready(function($) {
     }
 });
 
+jQuery(document).ready(function($) {
+    function fetchMqttMessages() {
+        $.ajax({
+            url: mqttClient.ajax_url,
+            method: 'POST',
+            data: {
+                action: 'get_mqtt_messages',
+                nonce: mqttClient.nonce
+            },
+            success: function(response) {
+                $('#mqtt-messages').html(response);
+            }
+        });
+    }
+
+    // Poll the server every 5 seconds
+    setInterval(fetchMqttMessages, 5000);
+
+    // Fetch messages immediately on load
+    fetchMqttMessages();
+});
