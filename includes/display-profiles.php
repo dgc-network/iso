18dad53795e9474292a5b1fceccd94a38a1f94ed
Rover Chen <rover.k.chen@gmail.com>
2024-06-05 16:22:13 +0800

    <?php echo display_mqtt_messages('mytopic/test');?>

--- includes/display-profiles.php
+++ includes/display-profiles.php
@@ -1324,7 +1324,8 @@ if (!class_exists('display_profiles')) {
                 <label for="password"><?php echo __( 'Password:', 'your-text-domain' );?></label>
                 <input type="text" id="password" value="<?php echo $password;?>" class="text ui-widget-content ui-corner-all" />
                 <label for="mqtt-messages"><?php echo __( 'Message received:', 'your-text-domain' );?></label>
-                <?php echo display_mqtt_messages('mytopic/test');?>
+                <?php //echo display_mqtt_messages('mytopic/test');?>
+                <?php echo display_mqtt_messages($client_id);?>
             </fieldset>
             <?php
             $html = ob_get_clean();
