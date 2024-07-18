8d4c43aa76395a3ec8baea40af999048ee32e4ea
Rover Chen <rover.k.chen@gmail.com>
2024-07-18 16:21:34 +0800

    } elseif ($field_type=='_employee') {                                             echo esc_html(get_the_title($field_value));

--- includes/display-documents.php
+++ includes/display-documents.php
@@ -783,7 +783,7 @@ if (!class_exists('display_documents')) {
                                         } elseif ($field_type=='_instrument') {
                                             echo esc_html(get_the_title($field_value));
                                         } elseif ($field_type=='_employee') {
-                                            echo get_the_title($field_value);
+                                            echo get_the_title((int) $field_value);
                                         } else {
                                             echo esc_html($field_value);
                                         }
