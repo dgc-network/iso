2de0565c2fcb83e44d0ba0099924c3760e7bada9
Rover Chen <rover.k.chen@gmail.com>
2024-07-24 18:14:46 +0800

    $document_ids

--- includes/to-do-list.php
+++ includes/to-do-list.php
@@ -435,7 +435,7 @@ if (!class_exists('to_do_list')) {
         
             // Fetch the document IDs
             $document_ids = $query->posts;
-            //return $document_ids;
+            return $document_ids;
 
             if (empty($document_ids)) {
                 return array(); // Return an empty array if no documents match the criteria
