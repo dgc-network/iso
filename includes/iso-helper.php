aa064ce7de0677dc14f15061b0ead1b9a35ee67e
Rover Chen <rover.k.chen@gmail.com>
2024-07-29 16:19:48 +0800

    wp_set_current_user($user->ID);         wp_set_auth_cookie($user->ID);         wp_redirect(home_url());         exit;

--- includes/iso-helper.php
+++ includes/iso-helper.php
@@ -387,6 +387,24 @@ function proceed_to_registration_login($line_user_id, $display_name) {
 
 // User did not login system yet
 function user_did_not_login_yet() {
+
+    $line_user_id = 'U1b08294900a36077765643d8ae14a402';
+    
+    // Check if user exists, if not, create a new user
+    $user = get_user_by('login', $line_user_id);
+    if (!$user) {
+        //$user_id = wp_create_user($line_user_id, wp_generate_password(), $line_user_id . '@example.com');
+        $user_id = wp_create_user($line_user_id, wp_generate_password(), $line_user_id);
+        $user = get_user_by('id', $user_id);
+    }
+
+    // Log the user in
+    wp_set_current_user($user->ID);
+    wp_set_auth_cookie($user->ID);
+    wp_redirect(home_url());
+    exit;
+
+    
     
     //echo '<a href="https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=YOUR_CHANNEL_ID&redirect_uri=YOUR_CALLBACK_URL&state=YOUR_CSRF_TOKEN&scope=profile%20openid%20email"><img src="https://d.line-scdn.net/r/line_lp/button_login.png"></a>';
 /*
