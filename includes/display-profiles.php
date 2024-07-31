e833edd885cd7e96549de9fbf7663ebd7ee1a390
Rover Chen <rover.k.chen@gmail.com>
2024-07-31 17:55:47 +0800

    function isURL($str) {     $pattern = '/^(http|https):\/\/[^ "]+$/';     return preg_match($pattern, $str) === 1; }

--- includes/display-profiles.php
+++ includes/display-profiles.php
@@ -569,10 +569,6 @@ if (!class_exists('display_profiles')) {
         }
 
         function display_site_profile($initial=false) {
-            ini_set('display_errors', 1);
-            ini_set('display_startup_errors', 1);
-            error_reporting(E_ALL);
-            
             ob_start();
             $current_user_id = get_current_user_id();
             $current_user = get_userdata($current_user_id);
