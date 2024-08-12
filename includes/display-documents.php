9cce356fa8a12889eb29c7116b6a253e78707419
Rover Chen <rover.k.chen@gmail.com>
2024-08-12 08:18:11 +0800

    </div>

--- includes/display-documents.php
+++ includes/display-documents.php
@@ -1033,9 +1033,13 @@ if (!class_exists('display_documents')) {
                 <?php
             } else {
                 ?>
+                <div style="display:flex; justify-content:space-between; margin:5px;">
                 <div>
                     <input type="button" id="duplicate-doc-report-<?php echo $report_id;?>" value="<?php echo __( 'Duplicate', 'your-text-domain' );?>" style="margin:3px;" />
+                </div>
+                <div style="text-align:right;">
                     <input type="button" id="doc-report-dialog-exit" value="<?php echo __( 'Exit', 'your-text-domain' );?>" style="margin:5px;" />
+                </div>
                 </div>
                 <?php
             }
