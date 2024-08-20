f8a937ba8fcaa6a99419646ed4dfa87f11c0432a
Rover Chen <rover.k.chen@gmail.com>
2024-08-20 09:39:17 +0800

    get_current_user_id()

--- includes/display-documents.php
+++ includes/display-documents.php
@@ -1665,7 +1665,7 @@ if (!class_exists('display_documents')) {
                             ?>
                             <label for="<?php echo esc_attr($field_name);?>"><?php echo esc_html($field_title);?></label>
                             <?php if ($default_value=='me') {?>
-                                <?php $user=get_userdata($field_value);?>
+                                <?php $user=get_userdata(get_current_user_id());?>
                                 <input type="text" id="<?php echo esc_attr($field_name);?>" value="<?php echo esc_html($user->display_name);?>" disabled class="text ui-widget-content ui-corner-all" />
                             <?php } else {?>
                                 <select multiple id="<?php echo esc_attr($field_name);?>" class="text ui-widget-content ui-corner-all multiple-select"><?php echo $cards_class->select_multiple_employees_options($field_value);?></select>
