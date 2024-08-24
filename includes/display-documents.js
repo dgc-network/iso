110f088efbafbbd48c86133efd79db7b6e4b2778
Rover Chen <rover.k.chen@gmail.com>
2024-08-25 06:39:40 +0800

    $('#result-container').text(response.html_contain);

--- includes/display-documents.js
+++ includes/display-documents.js
@@ -146,7 +146,9 @@ jQuery(document).ready(function($) {
                 _doc_id: doc_id,
             },
             success: function (response) {
-                $('#result-container').html(response.html_contain);
+                //$('#result-container').html(response.html_contain);
+                $('#result-container').text(response.html_contain);
+
                 activate_document_dialog_data(doc_id);
                 activate_published_document_data(doc_id);
                 activate_doc_report_list_data(doc_id);
