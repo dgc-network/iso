64d7c36ca82e479e2f5e5a27b3c77b7b699fcb06
Rover Chen <rover.k.chen@gmail.com>
2024-08-24 22:30:26 +0800

    ob_get_clean()

--- includes/display-documents.php
+++ includes/display-documents.php
@@ -350,7 +350,7 @@ if (!class_exists('display_documents')) {
         
         function display_document_dialog($doc_id=false) {
             ob_start();
-            //header('Content-Type: text/html; charset=utf-8');
+            header('Content-Type: text/html; charset=utf-8');
             $profiles_class = new display_profiles();
             $todo_class = new to_do_list();
             $cards_class = new erp_cards();
