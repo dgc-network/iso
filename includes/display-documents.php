ce059e9ec879ac0ea3157e91c16ce2ae2b320365
Rover Chen <rover.k.chen@gmail.com>
2024-08-26 23:34:37 +0800

    json_encode(

--- includes/display-documents.php
+++ includes/display-documents.php
@@ -1171,7 +1171,6 @@ if (!class_exists('display_documents')) {
             $default_value = get_post_meta($field_id, 'default_value', true);
             $field_name = get_post_meta($field_id, 'field_name', true);
             $field_value = $_POST[$field_name];
-            //update_post_meta($report_id, $field_name, $field_value);
 
             // additional field-name
             if ($field_type=='_employees'){
@@ -1203,7 +1202,6 @@ if (!class_exists('display_documents')) {
                     }    
                 }
                 update_post_meta($report_id, '_employees', $employee_ids);
-                //update_post_meta($report_id, $field_name, json_decode($field_value, true));
             }
 
             if ($field_type=='_document'){
@@ -1623,9 +1621,6 @@ if (!class_exists('display_documents')) {
                             <?php if ($default_value=='me') {?>
                                 <?php $user=get_userdata(get_current_user_id());?>
                                 <input type="hidden" id="<?php echo esc_attr($field_name);?>" value="<?php echo json_encode(array($user->ID));?>" />
-                                <?php /*
-                                <input type="hidden" id="<?php echo esc_attr($field_name); ?>" value="<?php echo esc_attr(json_encode($field_value)); ?>" />
-                                */?>
                                 <input type="text" value="<?php echo esc_html($user->display_name);?>" disabled class="text ui-widget-content ui-corner-all" />
                             <?php } else {?>
                                 <select multiple id="<?php echo esc_attr($field_name);?>" class="text ui-widget-content ui-corner-all multiple-select"><?php echo $cards_class->select_multiple_employees_options($field_value);?></select>
