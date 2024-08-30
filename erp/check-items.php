518950faba11697127b4c4a2daf863673e01561a
Rover Chen <rover.k.chen@gmail.com>
2024-08-30 10:13:33 +0800

    $documents_class

--- erp/check-items.php
+++ erp/check-items.php
@@ -475,8 +475,9 @@ if (!class_exists('check_items')) {
             $response['html_contain'] = $check_item_list;
             $report_id = sanitize_text_field($_POST['_report_id']);
             $doc_id = get_post_meta($report_id, 'doc_id', true);
-            $response['doc_fields'] = $this->get_doc_field_keys($doc_id);    
-    wp_send_json($response);
+            $documents_class = new display_documents();
+            $response['doc_fields'] = $documents_class->get_doc_field_keys($doc_id);    
+            wp_send_json($response);
         }
 
         // iso-category
