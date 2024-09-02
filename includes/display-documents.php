6382b12fb98eb456b8956a8ee5ac1d555ba26be7
Rover Chen <rover.k.chen@gmail.com>
2024-09-02 22:08:10 +0800

    ?>                                 <label for="<?php echo esc_attr($field_name);?>"><?php echo esc_html($field_title);?></label>                                 <select id="<?php echo esc_attr($field_name);?>" class="text ui-widget-content ui-corner-all"><?php echo $items_class->select_sub_item_options($field_value, $category_id);?></select>                                 <?php

--- includes/display-documents.php
+++ includes/display-documents.php
@@ -1145,7 +1145,7 @@ if (!class_exists('display_documents')) {
 
                         if ($field_type=='_sub') {
                             $parts = explode('=', $default_value);
-                            $sub_key = $parts[0]; // _category
+                            $sub_key = $parts[0]; // _embedded, _order_item, _select
                             $sub_value = $parts[1]; // 1724993477
             
                             if ($sub_key=='_embedded'||$sub_key=='_category') {
@@ -1246,7 +1246,7 @@ if (!class_exists('display_documents')) {
 
             if ($field_type=='_sub') {
                 $parts = explode('=', $default_value);
-                $sub_key = $parts[0]; // _category
+                $sub_key = $parts[0]; // _embedded, _order_item, _select
                 $sub_value = $parts[1]; // 1724993477
 
                 if ($sub_key=='_embedded'||$sub_key=='_category') {
@@ -1659,13 +1659,13 @@ if (!class_exists('display_documents')) {
                             break;
 
                         case ($field_type=='_sub'):
+                            $items_class = new sub_items();
                             $parts = explode('=', $default_value);
                             $sub_key = $parts[0]; // _embedded, _order_item, _select
                             $sub_value = $parts[1]; // 1724993477
             
                             if ($sub_key=='_embedded'||$sub_key=='_category') {
                                 if ($sub_value) {
-                                    $items_class = new sub_items();
                                     $category_id = $items_class->get_sub_category_post_id_by_code($sub_value);
                                     ?>
                                     <label for="<?php echo esc_attr($field_name);?>"><?php echo esc_html(get_the_title($category_id));?></label>
@@ -1720,13 +1720,17 @@ if (!class_exists('display_documents')) {
                                     </div>
                                     <?php
                                 }
-                            } else {
-                                $category_id = get_post_meta($report_id, '_sub_category', true);
-                                ?>
-                                <label for="<?php echo esc_attr($field_name);?>"><?php echo esc_html($field_title);?></label>
-                                <select id="<?php echo esc_attr($field_name);?>" class="text ui-widget-content ui-corner-all"><?php echo $items_class->select_sub_item_options($field_value, $category_id);?></select>
-                                <?php    
                             }
+                            if ($sub_key=='_select') {
+                                if ($sub_value) {
+                                    $category_id = $items_class->get_sub_category_post_id_by_code($sub_value);
+                                    ?>
+                                    <label for="<?php echo esc_attr($field_name);?>"><?php echo esc_html($field_title);?></label>
+                                    <select id="<?php echo esc_attr($field_name);?>" class="text ui-widget-content ui-corner-all"><?php echo $items_class->select_sub_item_options($field_value, $category_id);?></select>
+                                    <?php
+                                }
+                            }    
+
                             break;
 
                         case ($field_type=='_document'):
