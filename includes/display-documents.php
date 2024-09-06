c13551d6ea1b980716714496f8654979dca61257
Rover Chen <rover.k.chen@gmail.com>
2024-09-06 14:12:31 +0800

    get_the_ID()

--- includes/display-documents.php
+++ includes/display-documents.php
@@ -1484,6 +1484,7 @@ if (!class_exists('display_documents')) {
                                                 } elseif ($prev_report_id) {
                                                     $field_value = get_post_meta($prev_report_id, $field_name.get_the_ID(), true);
                                                 }
+                                                echo 'sub_item_id:'.get_the_ID().' report_id:'.$report_id.' prev_report_id:'.$prev_report_id.' field_value:'.$field_value.' field_name:'.$field_name.'<br>';
                                                 $items_class->get_sub_item_contains(get_the_ID(), $field_name, $field_value);
                                             endwhile;
                                             wp_reset_postdata();
