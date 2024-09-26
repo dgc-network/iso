f2d24fa1570dbfc281e3d87d91ef27b8ece2f9a0
Rover Chen <rover.k.chen@gmail.com>
2024-09-26 13:22:36 +0800

    display_iso_statement

--- includes/iso-helper.php
+++ includes/iso-helper.php
@@ -6,7 +6,7 @@ if (!defined('ABSPATH')) {
 //require_once plugin_dir_path( __FILE__ ) . 'to-do-list.php';
 require_once plugin_dir_path( __FILE__ ) . 'display-profiles.php';
 require_once plugin_dir_path( __FILE__ ) . 'erp-cards.php';
-//require_once plugin_dir_path( __FILE__ ) . 'subforms.php';
+require_once plugin_dir_path( __FILE__ ) . 'subforms.php';
 
 function admin_enqueue_scripts_and_styles() {
     $version = '1.0.0.'.time(); // Update this version number when you make changes
