af050c2e010dccb68bb74ff8f5c0a5f2a66e8107
Rover Chen <rover.k.chen@gmail.com>
2024-09-26 11:05:10 +0800

    display_iso_statement

--- includes/iso-helper.php
+++ includes/iso-helper.php
@@ -2,9 +2,9 @@
 if (!defined('ABSPATH')) {
     exit;
 }
-require_once plugin_dir_path( __FILE__ ) . 'display-documents.php';
+//require_once plugin_dir_path( __FILE__ ) . 'display-documents.php';
 //require_once plugin_dir_path( __FILE__ ) . 'to-do-list.php';
-//require_once plugin_dir_path( __FILE__ ) . 'display-profiles.php';
+require_once plugin_dir_path( __FILE__ ) . 'display-profiles.php';
 //require_once plugin_dir_path( __FILE__ ) . 'erp-cards.php';
 //require_once plugin_dir_path( __FILE__ ) . 'subforms.php';
 
