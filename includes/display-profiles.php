c1f60e775807399a564bbfc4b95439d799fbf7c8
Rover Chen <rover.k.chen@gmail.com>
2024-10-15 15:56:40 +0800

    iso-category

--- includes/display-profiles.php
+++ includes/display-profiles.php
@@ -166,7 +166,7 @@ if (!class_exists('display_profiles')) {
             }
         }
         //add_action('init', 'migrate_subform_code_to_embedded_code');
-        
+
         function migrate_subform_id_to_embedded_id() {
             // Query all posts of post type "embedded"
             $args = array(
@@ -195,7 +195,36 @@ if (!class_exists('display_profiles')) {
                 wp_reset_postdata();
             }
         }
+
+        function migrate_subform_meta_to_embedded_meta() {
+            // Query all posts of post type "embedded"
+            $args = array(
+                'post_type'      => 'iso-category',
+                'posts_per_page' => -1, // Retrieve all posts
+                'post_status'    => 'any',
+                'meta_key'       => 'subform', // Only query posts with 'subform_code'
+            );
+            $query = new WP_Query($args);
         
+            if ($query->have_posts()) {
+                while ($query->have_posts()) {
+                    $query->the_post();
+        
+                    // Get the old 'subform_code' meta value
+                    $old_meta_value = get_post_meta(get_the_ID(), 'subform', true);
+        
+                    if ($old_meta_value) {
+                        // Update the meta to use 'embedded_code' instead
+                        update_post_meta(get_the_ID(), 'embedded', $old_meta_value);
+        
+                        // Optionally, delete the old 'subform_code' meta to avoid duplication
+                        delete_post_meta(get_the_ID(), 'subform');
+                    }
+                }
+                wp_reset_postdata();
+            }
+        }
+
         // my-profile scripts
         function display_my_profile() {
             ob_start();
