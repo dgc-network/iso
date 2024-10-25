9769ab1bd97f8a55d28f28cd42b30714fd061cae
roverchen <rover.k.chen@gmail.com>
2024-10-25 15:15:16 +0800

    echo

--- includes/display-profiles.php
+++ includes/display-profiles.php
@@ -409,7 +409,7 @@ if (!class_exists('display_profiles')) {
             $frequence_report_start_time = get_post_meta($action_id, 'frequence_report_start_time', true);
             ?>
             <div>
-                <h4><?php '「'.echo get_the_title($doc_id).'」工作，'.'「'.get_the_title($action_id).'」動作'.$is_authorized;?></h4>
+                <h4><?php echo '「'.get_the_title($doc_id).'」工作，'.'「'.get_the_title($action_id).'」動作'.$is_authorized;?></h4>
                 <input type="hidden" id="is-action-authorized" value="<?php echo $is_action_authorized;?>" />
                 <label for="frequence-report-setting"><?php echo __( '循環表單啟動設定', 'your-text-domain' );?></label>
                 <select id="frequence-report-setting" class="text ui-widget-content ui-corner-all"><?php echo $todo_class->select_frequence_report_setting_option($frequence_report_setting);?></select>
