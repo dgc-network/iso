9798b3455265c8ae7ce26025a05f1ea3db09907d
roverchen <rover.k.chen@gmail.com>
2024-11-06 07:41:27 +0800

    action_id

--- includes/display-documents.php
+++ includes/display-documents.php
@@ -2,11 +2,11 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
+/*
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
+*/
 if (!class_exists('display_documents')) {
     class display_documents {
         // Class constructor
