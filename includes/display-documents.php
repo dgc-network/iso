bd94ca51a1a8a070ab1d05ed08627bc59957a217
roverchen <rover.k.chen@gmail.com>
2024-11-11 20:00:46 +0800

    <label for="<?php the_ID();?>"><?php echo $doc_title.'('.$doc_number.')'.get_the_ID();?></label>

--- includes/display-documents.php
+++ includes/display-documents.php
@@ -1975,7 +1975,7 @@ if (!class_exists('display_documents')) {
                                     ?>
                                     <div>
                                         <input type="checkbox" class="copy-document-class" id="<?php the_ID();?>" checked />
-                                        <label for="<?php the_ID();?>"><?php echo $doc_title.'('.$doc_number.')';?></label>
+                                        <label for="<?php the_ID();?>"><?php echo $doc_title.'('.$doc_number.')'.get_the_ID();?></label>
                                     </div>
                                     <?php
                                 endwhile;
