f25c2e08676be004bbff377ff9cf75cfcb300157
roverchen <rover.k.chen@gmail.com>
2024-12-05 13:29:44 +0800

    $default_value = $user_id;

--- includes/display-documents.php
+++ includes/display-documents.php
@@ -1497,14 +1497,9 @@ if (!class_exists('display_documents')) {
             }
             // Get and sanitize the field name and default value
             $default_value = sanitize_text_field(get_post_meta($field_id, 'default_value', true));
-            // Check if the default value should be 'today'
-            if ($default_value === 'today') {
-                $default_value = wp_date('Y-m-d', time()); // Set default value to today's date
-            }
-            // Check if the default value should be the current user ID
-            if ($default_value === 'me') {
-                $default_value = $user_id; // Set default value to an array with the current user ID
-            }
+            if ($default_value === 'today') $default_value = wp_date('Y-m-d', time());
+            if ($default_value === 'me') $default_value = $user_id;
+            if ($default_value=='_post_number') $default_value=time();
 
             $field_type = sanitize_text_field(get_post_meta($field_id, 'field_type', true));
             if (in_array($field_type, array('_embedded', '_planning', '_select')) && $default_value) {
@@ -1513,7 +1508,6 @@ if (!class_exists('display_documents')) {
             }
             $get_system_doc_id = $this->get_system_doc_id($field_type);
             if ($get_system_doc_id) {
-                if ($default_value=='_post_number') $default_value=time();
                 if ($field_type=='_employee' && $default_value=='me') $default_value=$user_id;
             }
 
