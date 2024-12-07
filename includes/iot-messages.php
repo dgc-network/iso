b5ba0baa0df7182d5c5c0cb892d02516bd4bcf18
roverchen <rover.k.chen@gmail.com>
2024-12-07 11:04:29 +0800

    'meta_key'       => 'device_number', // Meta key for sorting                 'orderby'        => 'meta_value', // Sort by meta value                 'order'          => 'ASC', // Sorting order (ascending)

--- includes/iot-messages.php
+++ includes/iot-messages.php
@@ -176,11 +176,11 @@ if (!class_exists('iot_messages')) {
                 'post_type'      => 'iot-device',
                 'posts_per_page' => 1,
                 'order'          => 'DESC',
-                'orderby'        => 'ID',
+                //'orderby'        => 'ID',
                 'post__lt'       => $current_device_id,
                 'meta_key'       => 'device_number', // Meta key for sorting
                 'orderby'        => 'meta_value', // Sort by meta value
-                'order'          => 'ASC', // Sorting order (ascending)
+                //'order'          => 'ASC', // Sorting order (ascending)
             );
             $query = new WP_Query($args);
             return $query->have_posts() ? $query->posts[0]->ID : null;
@@ -191,11 +191,11 @@ if (!class_exists('iot_messages')) {
                 'post_type'      => 'iot-device',
                 'posts_per_page' => 1,
                 'order'          => 'ASC',
-                'orderby'        => 'ID',
+                //'orderby'        => 'ID',
                 'post__gt'       => $current_device_id,
                 'meta_key'       => 'device_number', // Meta key for sorting
                 'orderby'        => 'meta_value', // Sort by meta value
-                'order'          => 'ASC', // Sorting order (ascending)
+                //'order'          => 'ASC', // Sorting order (ascending)
             );
             $query = new WP_Query($args);
             return $query->have_posts() ? $query->posts[0]->ID : null;
