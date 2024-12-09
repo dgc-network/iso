b76d073e4929982a572e3fa2566256e6caa7cd13
roverchen <rover.k.chen@gmail.com>
2024-12-09 21:47:39 +0800

    <label for="site-jobs"><?php echo __( '工作職掌：', 'your-text-domain' );?></label>

--- includes/display-profiles.php
+++ includes/display-profiles.php
@@ -591,7 +591,14 @@ if (!class_exists('display_profiles')) {
                 <label for="unified-number"><?php echo __( '統一編號：', 'your-text-domain' );?></label>
                 <input type="text" id="unified-number" value="<?php echo $unified_number;?>" class="text ui-widget-content ui-corner-all" />
 
-                <label for="site-jobs"><?php echo __( '工作職掌：', 'your-text-domain' );?></label>
+                
+                <div style="display:flex; justify-content:space-between; margin:5px;">
+                <div><label for="site-jobs"><?php echo __( '工作職掌：', 'your-text-domain' );?></label></div>
+                <div style="text-align: right">
+                    <input type="text" id="search-site-job" style="display:inline" placeholder="Search..." />
+                </div>
+                </div>
+
                 <?php echo $this->display_site_job_list();?>
 
             </fieldset>
