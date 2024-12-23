01dcdcff4243d66e31646a47164a2e7afd760feb
roverchen <rover.k.chen@gmail.com>
2024-12-23 22:56:18 +0800

    . "</div>"

--- services/gemini-api.php
+++ services/gemini-api.php
@@ -163,8 +163,8 @@ if (!class_exists('gemini_api')) {
 
             // Combine the CSS and content in a div
             $styled_html = $css . "<div class='content'>" . $content;
-            $styled_html .= '<input type="text" id="ask-gemini" class="text ui-widget-content ui-corner-all" />'
-            $styled_html .= "</div>"
+            $styled_html .= '<input type="text" id="ask-gemini" class="text ui-widget-content ui-corner-all" />';
+            $styled_html .= "</div>";
 
             return $styled_html;
         }
