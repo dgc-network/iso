016825cac23ca0a4cf11bee3e7d8a1c6de80601c
roverchen <rover.k.chen@gmail.com>
2025-01-02 15:40:53 +0800

    || current_user_can('administrator')

--- includes/iso-helper.php
+++ includes/iso-helper.php
@@ -51,9 +51,11 @@ function display_iso_helper_logo() {
 }
 
 function is_site_admin($user_id=false, $site_id=false) {
-    //if (current_user_can('administrator')) return true;
     // Get the current user ID
-    if (!$user_id) $user_id = get_current_user_id();
+    if (!$user_id) {
+        $user_id = get_current_user_id();
+        if (current_user_can('administrator')) return true;
+    }
     if (!$site_id) $site_id = get_user_meta($user_id, 'site_id', true);
     // Get the user's site_admin_ids as an array
     $site_admin_ids = get_user_meta($user_id, 'site_admin_ids', true);
