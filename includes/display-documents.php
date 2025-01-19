8c1ee4ab0adf5fdc71b2514ac5cea67b7f6235e8
roverchen <rover.k.chen@gmail.com>
2025-01-19 22:38:07 +0800

    get_the_ID()

--- includes/display-documents.php
+++ includes/display-documents.php
@@ -1441,7 +1441,7 @@ if (!class_exists('display_documents')) {
 
             // Get system document options
             $system_doc_query = $this->get_system_doc_query();
-
+/*
             if ($system_doc_query instanceof WP_Query && $system_doc_query->have_posts()) {
                 // Loop through the query results
                 while ($system_doc_query->have_posts()) {
