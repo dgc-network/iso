f7d110b8fcef57b002709d5dfcf33bfaef408bb8
roverchen <rover.k.chen@gmail.com>
2025-01-19 23:16:11 +0800

    $field_types = array_merge($field_types, $system_doc_array);

--- includes/display-documents.php
+++ includes/display-documents.php
@@ -1441,7 +1441,7 @@ if (!class_exists('display_documents')) {
 
 
             // Get system document options
-            $system_doc_array = $this->get_system_doc_query();
+            //$system_doc_array = $this->get_system_doc_query();
             //$field_types = array_merge($field_types, $system_doc_array);
 
 
