59c6ddbcb28fba2952e085ca7059f19098fd1a61
roverchen <rover.k.chen@gmail.com>
2025-02-16 06:54:47 +0800

    current

--- includes/display-documents.php
+++ includes/display-documents.php
@@ -498,7 +498,7 @@ if (!class_exists('display_documents')) {
                                     $next_job = get_post_meta($action_id, 'next_job', true);
                                     $next_job_title = get_the_title($next_job);
                                     //$next_job_title = get_post_meta($next_job, 'doc_title', true);
-                                    if (empty($next_job_title)) $next_job_title = __( 'Released', 'textdomain' );
+                                    if (empty($next_job_title)) $next_job_title = __( 'Send Notification via Line', 'textdomain' );
                                     //if (empty($next_job_title)) $next_job_title = get_post_meta($next_job, 'doc_title', true);
                                     $is_doc_report = get_post_meta($doc_id, 'is_doc_report', true);
                                     if ($next_job==-1) $next_job_title = __( 'Released', 'textdomain' );
