8e49a3f80e3c50cfb2c9e144dc54ebe03dbe55ec
roverchen <rover.k.chen@gmail.com>
2025-03-17 14:12:13 +0800

    $args['meta_query'][] =

--- includes/display-documents.php
+++ includes/display-documents.php
@@ -226,7 +226,7 @@ if (!class_exists('display_documents')) {
             <?php
         }
         
-        function retrieve_document_data($paged=1, $is_doc_report=false) {
+        function retrieve_document_data($paged=1, $is_doc_report=2) {
             $current_user_id = get_current_user_id();
             $site_id = get_user_meta($current_user_id, 'site_id', true);
             $args = array(
